3ca55dfe2e00dda53c256787403c3ebe30e21ad8
ERWANS2003 <126349551+ERWANS2003@users.noreply.github.com>
2026-09-14 13:28:43 +0000

    Gallery images: use absolute paths instead of asset() helper

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -291,78 +291,78 @@
             <p class="lead">{{ __('site.gallery_lead') }}</p>
             <div class="gallery-grid" id="gallery-grid">
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/image-8-min-scaled.jpg') }}" data-lightbox-src="{{ asset('images/gallery/image-8-min-scaled.jpg') }}">
-                        <img src="{{ asset('images/gallery/image-8-min-scaled.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/image-8-min-scaled.jpg" data-lightbox-src="/images/gallery/image-8-min-scaled.jpg">
+                        <img src="/images/gallery/image-8-min-scaled.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/img1.jpeg') }}" data-lightbox-src="{{ asset('images/gallery/img1.jpeg') }}">
-                        <img src="{{ asset('images/gallery/img1.jpeg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/img1.jpeg" data-lightbox-src="/images/gallery/img1.jpeg">
+                        <img src="/images/gallery/img1.jpeg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/img2.jpeg') }}" data-lightbox-src="{{ asset('images/gallery/img2.jpeg') }}">
-                        <img src="{{ asset('images/gallery/img2.jpeg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/img2.jpeg" data-lightbox-src="/images/gallery/img2.jpeg">
+                        <img src="/images/gallery/img2.jpeg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/img3.jpeg') }}" data-lightbox-src="{{ asset('images/gallery/img3.jpeg') }}">
-                        <img src="{{ asset('images/gallery/img3.jpeg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/img3.jpeg" data-lightbox-src="/images/gallery/img3.jpeg">
+                        <img src="/images/gallery/img3.jpeg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/IMG_5184-1.jpg') }}" data-lightbox-src="{{ asset('images/gallery/IMG_5184-1.jpg') }}">
-                        <img src="{{ asset('images/gallery/IMG_5184-1.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/IMG_5184-1.jpg" data-lightbox-src="/images/gallery/IMG_5184-1.jpg">
+                        <img src="/images/gallery/IMG_5184-1.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/IMG_5187.jpg') }}" data-lightbox-src="{{ asset('images/gallery/IMG_5187.jpg') }}">
-                        <img src="{{ asset('images/gallery/IMG_5187.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/IMG_5187.jpg" data-lightbox-src="/images/gallery/IMG_5187.jpg">
+                        <img src="/images/gallery/IMG_5187.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/IMG_5188.jpg') }}" data-lightbox-src="{{ asset('images/gallery/IMG_5188.jpg') }}">
-                        <img src="{{ asset('images/gallery/IMG_5188.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/IMG_5188.jpg" data-lightbox-src="/images/gallery/IMG_5188.jpg">
+                        <img src="/images/gallery/IMG_5188.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/Impact-positif-sur-lenvironnemnet-min-scaled-e1730914496421.webp') }}" data-lightbox-src="{{ asset('images/gallery/Impact-positif-sur-lenvironnemnet-min-scaled-e1730914496421.webp') }}">
-                        <img src="{{ asset('images/gallery/Impact-positif-sur-lenvironnemnet-min-scaled-e1730914496421.webp') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/Impact-positif-sur-lenvironnemnet-min-scaled-e1730914496421.webp" data-lightbox-src="/images/gallery/Impact-positif-sur-lenvironnemnet-min-scaled-e1730914496421.webp">
+                        <img src="/images/gallery/Impact-positif-sur-lenvironnemnet-min-scaled-e1730914496421.webp" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/karma1.jpg') }}" data-lightbox-src="{{ asset('images/gallery/karma1.jpg') }}">
-                        <img src="{{ asset('images/gallery/karma1.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/karma1.jpg" data-lightbox-src="/images/gallery/karma1.jpg">
+                        <img src="/images/gallery/karma1.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/karma123-min-1.jpg') }}" data-lightbox-src="{{ asset('images/gallery/karma123-min-1.jpg') }}">
-                        <img src="{{ asset('images/gallery/karma123-min-1.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/karma123-min-1.jpg" data-lightbox-src="/images/gallery/karma123-min-1.jpg">
+                        <img src="/images/gallery/karma123-min-1.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/karma2-min.jpg') }}" data-lightbox-src="{{ asset('images/gallery/karma2-min.jpg') }}">
-                        <img src="{{ asset('images/gallery/karma2-min.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/karma2-min.jpg" data-lightbox-src="/images/gallery/karma2-min.jpg">
+                        <img src="/images/gallery/karma2-min.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/karmareboi.jpg') }}" data-lightbox-src="{{ asset('images/gallery/karmareboi.jpg') }}">
-                        <img src="{{ asset('images/gallery/karmareboi.jpg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/karmareboi.jpg" data-lightbox-src="/images/gallery/karmareboi.jpg">
+                        <img src="/images/gallery/karmareboi.jpg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/WhatsApp-Image-2024-07-11-at-04.22.32-2.jpeg') }}" data-lightbox-src="{{ asset('images/gallery/WhatsApp-Image-2024-07-11-at-04.22.32-2.jpeg') }}">
-                        <img src="{{ asset('images/gallery/WhatsApp-Image-2024-07-11-at-04.22.32-2.jpeg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/WhatsApp-Image-2024-07-11-at-04.22.32-2.jpeg" data-lightbox-src="/images/gallery/WhatsApp-Image-2024-07-11-at-04.22.32-2.jpeg">
+                        <img src="/images/gallery/WhatsApp-Image-2024-07-11-at-04.22.32-2.jpeg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/WhatsApp-Image-2024-07-12-at-12.51.26.jpeg') }}" data-lightbox-src="{{ asset('images/gallery/WhatsApp-Image-2024-07-12-at-12.51.26.jpeg') }}">
-                        <img src="{{ asset('images/gallery/WhatsApp-Image-2024-07-12-at-12.51.26.jpeg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/WhatsApp-Image-2024-07-12-at-12.51.26.jpeg" data-lightbox-src="/images/gallery/WhatsApp-Image-2024-07-12-at-12.51.26.jpeg">
+                        <img src="/images/gallery/WhatsApp-Image-2024-07-12-at-12.51.26.jpeg" alt="Gallery image">
                     </a>
                 </figure>
                 <figure class="gallery-item">
-                    <a class="gallery-media" href="{{ asset('images/gallery/WhatsApp-Image-2024-07-12-at-12.51.27.jpeg') }}" data-lightbox-src="{{ asset('images/gallery/WhatsApp-Image-2024-07-12-at-12.51.27.jpeg') }}">
-                        <img src="{{ asset('images/gallery/WhatsApp-Image-2024-07-12-at-12.51.27.jpeg') }}" alt="Gallery image">
+                    <a class="gallery-media" href="/images/gallery/WhatsApp-Image-2024-07-12-at-12.51.27.jpeg" data-lightbox-src="/images/gallery/WhatsApp-Image-2024-07-12-at-12.51.27.jpeg">
+                        <img src="/images/gallery/WhatsApp-Image-2024-07-12-at-12.51.27.jpeg" alt="Gallery image">
                     </a>
                 </figure>
             </div>
